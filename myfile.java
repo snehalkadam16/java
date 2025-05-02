f3caed50a12171a18f6c98973c9901b4728dd066
sk <sk@gmail.com>
2025-05-02 15:13:55 +0000

    modify

--- myfile.java
+++ myfile.java
@@ -1,7 +1,7 @@
 class myfile
 {
-    public static void main(String[] args)
-    {
-        System.out.println(“Hello,world”);
-    }
+ public static void main(String[] args)
+  {
+    System.out.println("Hello,world");
+  }
 }
